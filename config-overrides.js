9df1059449e078c076745273869267b2fb074df5
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2024-12-02 17:09:38 +0700

    easeifytunes_UPDATED-1_FIX-5

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,4 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 const { override, useBabelRc } = require("customize-cra");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = override(useBabelRc(), (config) => {
-  config.plugins.push(
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "src/assets/videos/**/*.{mp4,avi,mov}",
-          to: "videos/[name][ext]",
-        },
-      ],
-    })
-  );
-
-  return config;
-});
+module.exports = override(useBabelRc());
--- src/Api/API_02.js
+++ src/Api/API_02.js
@@ -1,4 +1,4 @@
-import mp4Type from "~/assets/videos/mp4Type";
+import videos from "~/assets/videosImport";
 
 const apiMoment = {
   getMoment: () => {
@@ -7,28 +7,19 @@ const apiMoment = {
         id: 0,
         date: `25-08-2024`,
         name: `THE PROCESS OF MAKING 12H03 - TOULIVER & MASTAL`,
-        link:
-          process.env.NODE_ENV === "production"
-            ? `/videos/MAKING 12H03 TOULIVER MASTAL.mp4`
-            : mp4Type[`MAKING 12H03 TOULIVER MASTAL`],
+        link: videos[`MAKING 12H03 TOULIVER MASTAL.mp4`],
       },
       {
         id: 1,
         date: `15-08-2024`,
         name: "THE PROCESS OF MAKING ĐÀO LIỄU - TOULIVER & TRIPLE D",
-        link:
-          process.env.NODE_ENV === "production"
-            ? `/videos/ĐÀO LIỄU TOULIVER TRIPLE D`
-            : mp4Type[`ĐÀO LIỄU TOULIVER TRIPLE D`],
+        link: videos[`ĐÀO LIỄU TOULIVER TRIPLE D.mp4`],
       },
       {
         id: 2,
         date: `10-08-2024`,
         name: `[BEAT BREAKDOWN] TRỐNG CƠM - TỰ LONG, SOOBIN, CƯỜNG SEVEN, (CÔNG DIỄN 1 ANH TRAI VƯỢT NGÀN CHÔNG GAI)`,
-        link:
-          process.env.NODE_ENV === "production"
-            ? `/videos/BEAT TRỐNG CƠM-ATVNTG.mp4`
-            : mp4Type[`BEAT TRỐNG CƠM-ATVNTG`],
+        link: videos[`BEAT TRỐNG CƠM-ATVNTG.mp4`],
       },
     ];
   },
