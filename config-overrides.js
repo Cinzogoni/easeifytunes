c17faa5f964d17533a0156cb73869280fea23eb1
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2024-12-02 16:34:27 +0700

    easeifytunes_UPDATED-1_FIX-4

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,18 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 const { override, useBabelRc } = require("customize-cra");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = override(useBabelRc());
+module.exports = override(useBabelRc(), (config) => {
+  config.plugins.push(
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/assets/videos/**/*.{mp4,avi,mov}",
+          to: "videos",
+        },
+      ],
+    })
+  );
+
+  return config;
+});
--- src/Api/API_02.js
+++ src/Api/API_02.js
@@ -7,19 +7,28 @@ const apiMoment = {
         id: 0,
         date: `25-08-2024`,
         name: `THE PROCESS OF MAKING 12H03 - TOULIVER & MASTAL`,
-        link: mp4Type[`MAKING 12H03 TOULIVER MASTAL`],
+        link:
+          process.env.NODE_ENV === "production"
+            ? `/videos/MAKING 12H03 TOULIVER MASTAL.mp4`
+            : mp4Type[`MAKING 12H03 TOULIVER MASTAL`],
       },
       {
         id: 1,
         date: `15-08-2024`,
         name: "THE PROCESS OF MAKING ĐÀO LIỄU - TOULIVER & TRIPLE D",
-        link: mp4Type[`ĐÀO LIỄU TOULIVER TRIPLE D`],
+        link:
+          process.env.NODE_ENV === "production"
+            ? `/videos/ĐÀO LIỄU TOULIVER TRIPLE D`
+            : mp4Type[`ĐÀO LIỄU TOULIVER TRIPLE D`],
       },
       {
         id: 2,
         date: `10-08-2024`,
         name: `[BEAT BREAKDOWN] TRỐNG CƠM - TỰ LONG, SOOBIN, CƯỜNG SEVEN, (CÔNG DIỄN 1 ANH TRAI VƯỢT NGÀN CHÔNG GAI)`,
-        link: mp4Type[`BEAT TRỐNG CƠM-ATVNTG`],
+        link:
+          process.env.NODE_ENV === "production"
+            ? `/videos/BEAT TRỐNG CƠM-ATVNTG.mp4`
+            : mp4Type[`BEAT TRỐNG CƠM-ATVNTG`],
       },
     ];
   },
